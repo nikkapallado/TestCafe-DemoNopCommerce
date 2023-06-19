f92e0402eaf3fca5b78c9dba42ef14589e73b5f7
nikkapallado <ntpallado@gmail.com>
2023-06-19 09:50:04 +0800

    feat: create home page object

---
+++ pages/HomePage.js
@@ -0,0 +1,34 @@
+import { Selector, t } from "testcafe";
+
+class HomePage{
+    constructor(){
+        this.subtitleHeader = Selector('h2').withText('Welcome to our store');
+        this.registerLink = Selector('a').withText('Register');
+        this.loginLink = Selector('a').withText('Log in');
+        this.shoppingCartLink = Selector('a').withText('Shopping cart');
+        this.wishlistLink = Selector('a').withText('Wishlist');
+        this.myAccountLink = Selector('a').withText('My Account');
+        this.logoutLink = Selector('a').withText('Log out');
+        this.currencyDropdown = Selector('select#customerCurrency');
+        this.searchInput = Selector('input#small-searchterms')
+    }
+
+    get productSearch(){
+        return Selector('input[id="small-searchterms"]');
+    }
+
+    async search(product){
+        await t
+            .typeText(this.productSearch, product)
+            .wait(3000)
+            .pressKey('enter');
+    }
+
+    async changeCurrency(currency){
+        await t
+            .click(this.currencyDropdown)
+            .click(Selector('option',{text: currency}));
+    }
+}
+
+export default new HomePage();
