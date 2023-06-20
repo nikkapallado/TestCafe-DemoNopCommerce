b07542b9ca2427d9ddc06f2404408875971e7a79
nikkapallado <ntpallado@gmail.com>
2023-06-21 04:42:30 +0800

    chore: apply cucumber html reporter

---
+++ cucumber-html-reporter.js
@@ -0,0 +1,26 @@
+var reporter = require('cucumber-html-reporter');
+
+var options = {
+        theme: 'bootstrap',
+        jsonFile: 'report/cucumber_html_report/cucumber_report.json',
+        output: 'report/cucumber_html_report/cucumber_report.html',
+        reportSuiteAsScenarios: true,
+        scenarioTimestamp: true,
+        launchReport: true,
+        metadata: {
+            "App Version":"0.3.2",
+            "Test Environment": "STAGING",
+            "Browser": "Chrome  54.0.2840.98",
+            "Platform": "Windows 10",
+            "Parallel": "Scenarios",
+            "Executed": "Remote"
+        },
+        failedSummaryReport: true,
+    };
+
+    reporter.generate(options);
+    
+
+    //more info on `metadata` is available in `options` section below.
+
+    //to generate consodilated report from multi-cucumber JSON files, please use `jsonDir` option instead of `jsonFile`. More info is available in `options` section below.
