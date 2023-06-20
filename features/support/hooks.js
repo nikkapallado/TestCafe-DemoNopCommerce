6c12c75409b608e395cc33f67b0e1ed7cfe64de3
nikkapallado <ntpallado@gmail.com>
2023-06-21 04:42:09 +0800

    chore: add support for cucumber test controller runner

---
+++ features/support/hooks.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+const createTestCafe = require('testcafe');
+const testControllerHolder = require('../support/testControllerHolder');
+const {AfterAll, setDefaultTimeout, Before, After, Status} = require('@cucumber/cucumber');
+const errorHandling = require('../support/errorHandling');
+const TIMEOUT = 20000;
+
+let isTestCafeError = false;
+let attachScreenshotToReport = null;
+let cafeRunner = null;
+let n = 0;
+
+function createTestFile() {
+    fs.writeFileSync('test.js',
+        'import errorHandling from "./features/support/errorHandling.js";\n' +
+        'import testControllerHolder from "./features/support/testControllerHolder.js";\n\n' +
+
+        'fixture("fixture")\n' +
+
+        'test\n' +
+        '("test", testControllerHolder.capture)')
+}
+
+function runTest(iteration, browser) {
+    createTestCafe()
+        .then(function(tc) {
+            cafeRunner = tc;
+            const runner = tc.createRunner();
+            return runner
+                .src('./test.js')
+                .screenshots('report/screenshots/', true)
+                .browsers(browser)
+                .run()
+                .catch(function(error) {
+                    console.error(error);
+                });
+        })
+        .then(function(report) {
+        });
+}
+
+
+setDefaultTimeout(TIMEOUT);
+
+Before(function() {
+    runTest(n, this.setBrowser());
+    createTestFile();
+    n += 1;
+    return this.waitForTestController.then(function(testController) {
+        return testController.maximizeWindow();
+    });
+});
+
+After(function() {
+    fs.unlinkSync('test.js');
+});
+
+After(async function(testCase) {
+    const world = this;
+    if (testCase.result.status === Status.FAILED) {
+        isTestCafeError = true;
+        attachScreenshotToReport = world.attachScreenshotToReport;
+        errorHandling.addErrorToController();
+        await errorHandling.ifErrorTakeScreenshot(testController)
+    }
+});
+
+AfterAll(function() {
+    let intervalId = null;
+
+    function waitForTestCafe() {
+        intervalId = setInterval(checkLastResponse, 500);
+    }
+
+    function checkLastResponse() {
+        if (testController.testRun.lastDriverStatusResponse === 'test-done-confirmation') {
+            cafeRunner.close();
+            process.exit();
+            clearInterval(intervalId);
+        }
+    }
+
+    for(let i=0; i <= n; i++){
+        waitForTestCafe();
+        testControllerHolder.free();
+    }
+});
+
+const getIsTestCafeError = function() {
+    return isTestCafeError;
+};
+
+const getAttachScreenshotToReport = function(path) {
+    return attachScreenshotToReport(path);
+};
+
+exports.getIsTestCafeError = getIsTestCafeError;
+exports.getAttachScreenshotToReport = getAttachScreenshotToReport;
