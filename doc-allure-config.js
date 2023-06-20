695d8f79b41d5405f50c7460d9f0ad9e13282b02
nikkapallado <ntpallado@gmail.com>
2023-06-21 06:05:51 +0800

    chore: added allure config

---
+++ doc-allure-config.js
@@ -0,0 +1,26 @@
+const DOC_ALLURE_CONFIG = {
+    CLEAN_REPORT_DIR: true, //set to false if you want to see history
+    COPY_HISTORY: true,
+    RESULT_DIR: '/report/allure_report/allure_results',
+    REPORT_DIR: '/report/allure_report/allure_results',
+    SCREENSHOT_DIR: '/report/allure_report',
+    META: {
+        STORY_ID: 'STORY',
+        TEST_ID: 'ID',
+        SEVERITY: 'SEVERITY',
+        TEST_RUN: 'TEST_RUN'
+    },
+    STORY_LABEL: 'JIRA Story Link',
+    STORY_URL: 'https://jira.example.ca/browse/{{ID}}',
+    TEST_LABEL: 'JIRA Test Link',
+    TEST_URL: 'https://jira.example.ca/secure/Tests.jspa#/testCase/{{ID}}',
+    labels: {
+        screenshotLabel: 'Screenshot',
+        browserLabel: 'Browser',
+        userAgentLabel: 'User Agent',
+        allureStartMessage: 'Allure reporter started...',
+        allureClosedMessage: 'Allure reporter closed...'
+    }
+};
+ 
+module.exports = DOC_ALLURE_CONFIG;
