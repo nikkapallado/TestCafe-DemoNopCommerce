48d14af6155dba8396e2e490e1f4950689be4025
nikkapallado <ntpallado@gmail.com>
2023-06-21 04:51:38 +0800

    refactor: fix error

--- features/support/world.js
+++ features/support/world.js
@@ -1,4 +1,4 @@
-const {setWorldConstructor} = require('cucumber');
+const {setWorldConstructor} = require('@cucumber/cucumber');
 const testControllerHolder = require('./testControllerHolder');
 const base64Img = require('base64-img');
 
