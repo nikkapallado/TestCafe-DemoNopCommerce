f05a3979946ddec049665bcf4937212c8468fd4b
nikkapallado <ntpallado@gmail.com>
2023-06-21 09:08:39 +0800

    chore: applied applitools eyes

--- test/RegistrationTest.js
+++ test/RegistrationTest.js
@@ -3,7 +3,9 @@ import HomePage from "../pages/HomePage.js";
 import RegisterPage from "../pages/RegisterPage.js";
 import LoginPage from "../pages/LoginPage.js";
 import CustomerPage from "../pages/CustomerPage.js";
-import RegistrationData from "../data/RegistrationData.json"
+import RegistrationData from "../data/RegistrationData.json";
+import Eyes from '@applitools/eyes-testcafe';
+const eyes = new Eyes();
 
 
 const URL = 'https://demo.nopcommerce.com/';
@@ -11,23 +13,41 @@ const getPageURL = ClientFunction(() => window.location.href);
 var randomNumber = Math.floor(Math.random() * 10000);
 
 fixture('Registration Fixture')
-    .meta({"type":"smoke"})    
-    .page(URL);
+    .meta({ "type": "smoke" })
+    .afterEach(async () => await eyes.close())
+    .after(async () => await eyes.waitForResults(true))
+    .page(URL); 
 
 test('Assert Home Page Test', async t => {
+    //Applitools Eyesawait 
+    await eyes.open({
+        appName: 'Demo Nop Commerce Website',
+        testName: 'Assert Home Page Test',
+        t
+    });
     await t
         .expect(getPageURL()).eql(URL)
         .takeScreenshot()
         .expect(HomePage.subtitleHeader.exists).ok();
+    await eyes.checkWindow('Home Page');
 });
 
-RegistrationData.forEach(data => { 
+RegistrationData.forEach(data => {
     var userEmail = `${randomNumber}${data.email}`
     test('Assert Registration and Login Test', async t => {
+        //Applitools Eyesawait 
+        await eyes.open({
+            appName: 'Demo Nop Commerce Website',
+            testName: 'Assert Registration and Login Test',
+            t
+        });
+        await eyes.checkWindow('Home Page');
         //Register
         await t
             .click(HomePage.registerLink)
-            .expect(getPageURL()).contains('/register')
+            .expect(getPageURL()).contains('/register');
+        await eyes.checkWindow('Registration Page');
+        await t
             .click(RegisterPage.genderOption.withText(data.gender))
             .typeText(RegisterPage.firstNameInput, data.firstname)
             .typeText(RegisterPage.lastNameInput, data.lastname);
@@ -39,6 +59,8 @@ RegistrationData.forEach(data => {
             .typeText(RegisterPage.passwordInput, data.password)
             .typeText(RegisterPage.confirmPasswordInput, data.password)
             .click(RegisterPage.registerButton)
+        await eyes.checkWindow('Success Page');
+        await t
             .expect(RegisterPage.successfulRegistrationConfirmation.exists).ok()
             //Login
             .click(HomePage.loginLink)
